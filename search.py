a2c552ebee75c814e2be56bd6511d43cb53a5cbb
zld <lidongzhu@gmail.com>
2024-09-12 16:03:12 +0800

    Add files via upload
    some fix

--- search.py
+++ search.py
@@ -17,24 +17,26 @@ else :
     if field in fields : #栏目存在才继续
         
         #准备结果输出文件，csv格式，文件名 result_关键词_项目.cvs
-        csv_writer = csv.DictWriter(open('result_'+name+'_'+field+'.csv','w', newline='', encoding = 'utf-8-sig'),fieldnames = fields)
-        csv_writer.writeheader() #写入项目标题
-        paths = os.walk(r'./') 
-        #主循环
-        for path , dir_lst, file_lst in paths: #当前文件夹所有文件遍历
-            for file_name in file_lst:
-                if file_name.endswith('csv')& (not ('result' in file_name)): #排除非cvs格式，以及结果输出文件
-                    #打开csv文件
-                    csv_reader = csv.DictReader(open(os.path.join(path,file_name),'r', encoding = 'utf-8-sig' ))
-                    for row in csv_reader: #遍历文件所有行
-                        if name in row[field] : #判断条件，项目中是否包括关键词
-                            count +=1           #计时加一
-                            print(count , row['案件名称'])  #打印到终端
-                            csv_writer.writerow(row)        #保存到结果文件
-        if count>0 :
-            print('总共',count, '行结果保存在：','result_'+name+'_'+field+'.csv')
-        else:
-            print('未找到符合要求的结果')
+        with open('result_'+name+'_'+field+'.csv','w', newline='', encoding = 'utf-8-sig') as result_file:
+            csv_writer = csv.DictWriter(result_file ,fieldnames = fields)
+            csv_writer.writeheader() #写入项目标题
+            paths = os.walk(r'./') 
+            #主循环
+            for path , dir_lst, file_lst in paths: #当前文件夹所有文件遍历
+                for file_name in file_lst:
+                    if file_name.endswith('csv')& (not ('result' in file_name)): #排除非cvs格式，以及结果输出文件
+                        #打开csv文件
+                        with open(os.path.join(path,file_name), mode = 'r', encoding = 'utf-8-sig' ) as csv_file:
+                            csv_reader = csv.DictReader(csv_file)
+                            for row in csv_reader: #遍历文件所有行
+                                if name in row[field] : #判断条件，项目中是否包括关键词
+                                    count +=1           #计数加一
+                                    print(count , row['案件名称'])  #打印结果到终端
+                                    csv_writer.writerow(row)        #保存结果到结果文件
+            if count>0 :
+                print('总共',count, '行结果保存在：','result_'+name+'_'+field+'.csv')
+            else:
+                print('未找到符合要求的结果')
     else:
         print(field, '是无效栏目' )
         print('有效栏目有：', fields)
